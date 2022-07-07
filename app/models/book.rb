9a9b3aa13c6fb616e8e143752135143fef3553ab
konnotakafumi4 <longwenjinye@gmail.com>
2022-07-07 01:05:03 +0000

    [Finish]投稿数表示（今日、昨日）

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,12 +4,14 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   has_many :book_comments, dependent: :destroy
-  
+
   has_many :view_counts, dependent: :destroy
 
   validates :title, presence:true
   validates :body, presence:true,length:{maximum:200}
 
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
 
   # 検索方法分岐
   def self.looks(search, word)
